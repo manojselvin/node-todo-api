884e49114f575b366231ba0c50096ad0a7dff3df
Manoj Selvin <manoj.selvin@yocket.in>
2018-09-15 22:50:51 +0530

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log(`Unable to connect to MongoDB server`, err);
+    }
+
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     completed: true
+    // }, {
+    //     $set: {
+    //         completed: false
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        name: 'Vinod Selvin'
+    }, {
+        $set: {
+            name: 'Manoj Selvin'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // db.close();
+});
